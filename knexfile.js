ecbd4a6225f12c7cc70a85668dad0dc0c15be879
MarleideFranca <marleidealves2002@yahoo.com.br>
2022-10-25 16:27:01 -0300

    procedures e triggers

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "Knex_test"
+      user: "postgres",
+      password: "rapha"
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+  onUpdateTrigger: table => `
+  CREATE TRIGGER ${table}_updated_at
+  BEFORE UPDATE ON ${table}
+  FOR EACH ROW
+  EXECUTE PROCEDURE on_update_timestamp();
+  `
+};
